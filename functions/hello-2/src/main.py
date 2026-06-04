0b0364c8fc86ff554052da53d7aafaee940a0397
Giovanni Martins <giovannidealmeidamartins@gmail.com>
2026-06-04 03:02:45 -0300

    chore: update function identifier in hello-2 main handler

--- functions/hello-2/src/main.py
+++ functions/hello-2/src/main.py
@@ -3,6 +3,6 @@ import json
 class MainHandler:
     def handle(req):
         return json.dumps({
-            "function": "hello-2",
+            "function": "hello-2-a",
             "input": req.body
         })
