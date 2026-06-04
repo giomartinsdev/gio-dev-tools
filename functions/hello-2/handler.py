0ee12284236a060108e297c851f0f924e2eb338e
Giovanni Martins <giovannidealmeidamartins@gmail.com>
2026-06-04 03:30:23 -0300

    chore: bump hello-2 function version to 1.0.7

--- functions/hello-2/handler.py
+++ functions/hello-2/handler.py
@@ -5,7 +5,7 @@ def handle(req):
     return json.dumps({
         "function": "hello-2",
         "input": req,
-        "version": "1.0.6"
+        "version": "1.0.7"
     })
 
 if __name__ == "__main__":
