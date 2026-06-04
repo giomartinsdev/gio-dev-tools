b908e18e5ad685f59209495d823174f7371d7021
Giovanni Martins <giovannidealmeidamartins@gmail.com>
2026-06-04 15:54:16 -0300

    chore: bump version to 1.0.24 and update request serialization in hello-fn

--- functions/hello-fn/src/main.py
+++ functions/hello-fn/src/main.py
@@ -8,14 +8,14 @@ def main(request: Request) -> Response:
     return Response(
         body={
             "message": "Hello from Python on OpenFaaS",
-            "version": "1.0.22",
+            "version": "1.0.24",
             "random_int": random.randint(1, 100),
             "df_head": pd.DataFrame({
                 "col1": [1, 2, 3, 4, 5],
                 "col2": [6, 7, 8, 9, 10],
                 "col3": [11, 12, 13, 14, 15]
             }).to_dict(orient="records"),
-            "request": request,
+            "request": request.to_dict(),
         },
         status_code=200,
     )
