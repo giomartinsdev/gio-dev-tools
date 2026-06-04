75b1eca7c6d82525fc421571a74f7463bdd9ac09
Giovanni Martins <giovannidealmeidamartins@gmail.com>
2026-06-04 02:27:13 -0300

    refactor: migrate function handlers from lambda-style functions to standalone HTTPServer implementations

--- functions/hello-2/handler.py
+++ functions/hello-2/handler.py
@@ -1,9 +1,26 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 
-def handle(event, context):
-    return json.dumps(
-        {
-            "function": "test-hello-2",
-            "input": json.loads(event.body) if event.body else {}
-        }
-    )
+class Handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers.get('Content-Length', 0))
+        body = self.rfile.read(content_length)
+        
+        try:
+            data = json.loads(body) if body else {}
+        except json.JSONDecodeError:
+            data = {}
+
+        response = json.dumps({"function": "hello-2", "input": data})
+        
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(response.encode())
+
+    def log_message(self, format, *args):
+        pass
+
+if __name__ == '__main__':
+    server = HTTPServer(('0.0.0.0', 5000), Handler)
+    server.serve_forever()
