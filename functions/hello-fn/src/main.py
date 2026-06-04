5c285ae0361c226b20b682ec659dc7dfa0bf16f1
Giovanni Martins <giovannidealmeidamartins@gmail.com>
2026-06-04 04:20:32 -0300

    feat: add pandas dependency and include mock dataframe sample in response body

--- functions/hello-fn/src/main.py
+++ functions/hello-fn/src/main.py
@@ -1,4 +1,12 @@
 import random
+import pandas as pd
+
+def generate_mock_df():
+    return pd.DataFrame({
+        "col1": [1, 2, 3, 4, 5],
+        "col2": [6, 7, 8, 9, 10],
+        "col3": [11, 12, 13, 14, 15]
+    })
 
 def main(event, context):
     headers = {
@@ -8,8 +16,9 @@ def main(event, context):
         "statusCode": 200,
         "body": {
             "message": "Hello from Python on OpenFaaS",
-            "version": "1.0.17",
-            "random_int": random.randint(1, 100)
+            "version": "1.0.18",
+            "random_int": random.randint(1, 100),
+            "df_head": generate_mock_df().head().to_dict()
         },
         "headers": headers
     }
